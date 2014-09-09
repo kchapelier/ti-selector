c9e0f2773b703cfe98b9de7b6d0975bf83d7501c
kchapelier <kevin.chapelier@gmail.com>
2014-09-09 13:10:10 +0200

    first working implementation of the query parser

--- src/lib/query-parser.js
+++ src/lib/query-parser.js
@@ -1,18 +1,20 @@
 var parser = (function() {
     "use strict";
-    
+
     var tokenizer = function(query) {
-        throw new Error('To implement');
-        
         var position = 0,
             length = query.length,
             tokens = [];
-        
+
+        var allowedCharactersForId = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890-_'.split(''),
+            allowedCharactersForClassName = allowedCharactersForId,
+            allowedCharactersForTagName = allowedCharactersForId;
+
         var readBasicToken = function(property, operand, allowedCharacters) {
             var token = { property : property, operand : operand, value : null },
-                value = '',
-                end = false;
-                
+                    value = '',
+                    end = false;
+
             while(position < length && !end) {
                 var character = query[position];
 
@@ -26,39 +28,47 @@ var parser = (function() {
             }
 
             token.value = value;
-            
+
             return token;
         };
-        
+
         var readTagName = function() {
-            return readBasicToken(
-                'tagname',
-                '=',
-                'a-zA-Z0-9-_'.join('')
-            );
+            return readBasicToken('tagname', '=', allowedCharactersForTagName);
         };
-        
+
         var readId = function() {
-            return readBasicToken(
-                'id',
-                '=',
-                'a-zA-Z0-9-_'.join('')
-            );
+            return readBasicToken('id', '=', allowedCharactersForId);
         };
-        
+
         var readClassName = function() {
-            return readBasicToken(
-                'classname',
-                '=',
-                'a-zA-Z0-9-_'.join('')
-            );
+            return readBasicToken('classname', '=', allowedCharactersForClassName);
         };
+
+        while(position < length) {
+            var character = query[position];
+
+            if(character === '.') {
+                position = position + 1;
+                tokens.push(readClassName());
+            } else if(character === '#') {
+                position = position + 1;
+                tokens.push(readId());
+            } else if(allowedCharactersForTagName.indexOf(character) >= 0) {
+                tokens.push(readTagName());
+            } else {
+                throw new Error('Unexpected character : "' + firstCharacter + '"');
+            }
+
+            position++;
+        }
+
+        return tokens;
     };
-    
+
     var parser = function(query) {
         var ruleSetList = [],
-            individualQueries = query.split(',');
-            
+                individualQueries = query.split(',');
+
         individualQueries.forEach(function(individualQuery) {
             individualQuery = individualQuery.trim();
             var ruleSet = tokenizer(individualQuery);
@@ -66,11 +76,11 @@ var parser = (function() {
                 ruleSetList.push(ruleSet);
             }
         });
-        
+
         return ruleSetList;
     };
-    
-    return parser; 
+
+    return parser;
 }());
 
 module.exports = parser;
