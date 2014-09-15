2f975e2df5057e4ef4eb19b42d3f471479126633
kchapelier <kevin.chapelier@gmail.com>
2014-09-15 20:41:41 +0200

    refactoring, moving the actual iteration in the iterator

--- src/lib/iterator.js
+++ src/lib/iterator.js
@@ -1,9 +1,9 @@
 var iterator = (function() {
     "use strict";
-    
+
     var getChildren = function(element) {
         var children = [];
-        
+
         if(element.children) {
             children = element.children;
         } else if(element.apiName === 'Ti.Api.TableViewSection') {
@@ -11,23 +11,41 @@ var iterator = (function() {
         } else if(element.apiName === 'Ti.Api.TableViewRow') {
             children = element.data;
         }
-        
+
         return children;
     };
-    
+
     var getParents = function(element) {
         var parents = [];
-        
+
         if(element.parent) {
             parents.push(element.parent);
         }
-        
+
         return parents;
     };
-    
+
+    var createWalkFunction = function(iterativeFunc) {
+        var self = function(root, func) {
+            var elements = iterativeFunc(root);
+
+            for(var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+
+                if(false === func(element)) {
+                    return;
+                }
+
+                self(element, func);
+            }
+        };
+
+        return self;
+    };
+
     return {
-        parents : getParents,
-        children : getChildren
+        walkParents : createWalkFunction(getParents),
+        walkChildren : createWalkFunction(getChildren)
     };
 }());
 
--- src/lib/selector.js
+++ src/lib/selector.js
@@ -1,129 +1,120 @@
+/* Submodules */
+
 var parser = require('./query-parser'),
     iterator = require('./iterator');
 
+/* Submodules end */
+
+
+
 var selector = (function() {
     "use strict";
-    
+
     var operands = {
         'match-class' : function(actual, expected) {
             return (actual && (new RegExp('(^| )' + expected + '($| )')).test(actual));
         },
         'match-tag' : function(actual, expected) {
             actual = actual.substr(actual.lastIndexOf('.') + 1);
-            
-            return actual.toLowerCase() == expected.toLowerCase();
+
+            return actual.toLowerCase() === expected.toLowerCase();
         },
         '=' : function(actual, expected) {
             return actual === expected;
         }
     };
-    
+
     var getMatchingFunction = function(query) {
         var type = typeof query,
             matchingFunction;
-        
+
         if(type === 'function') {
             matchingFunction = query;
         } else if(type === 'string') {
             var ruleSetList = parser(query);
-            
+
             matchingFunction = function(element) {
                 for(var i = 0; i < ruleSetList.length; i++) {
                     var ruleSet = ruleSetList[i];
-                    
+
                     var matching = true;
-                    
+
                     for(var j = 0; j < ruleSet.length && matching; j++) {
                         var property = ruleSet[j].property,
                             value = ruleSet[j].value,
                             operand = ruleSet[j].operand; //willfully ignored so far
-                        
+
                         if(property === 'class') {
                             property = 'className';
                         } else if(property === 'tagname') {
                             property = 'apiName';
                         }
-                        
+
                         matching = operands[operand] && element[property] && operands[operand](element[property], value);
                     }
-                    
+
                     if(matching) {
                         return true;
                     }
                 }
-                
+
                 return false;
             };
         } else {
             throw new Error('Unexpected type of query : ' + type);
         }
-        
+
         return matchingFunction;
     };
-    
+
     var getElements = function(root, query, limit) {
         var queryFunction = getMatchingFunction(query),
             results = [];
-            
-        var iterate = function(root) {
-            var elements = iterator.children(root);
-            
-            for(var i = 0; i < elements.length; i++) {
-                if(limit && results.length >= limit) {
-                    return;
-                }
-                
-                var element = elements[i];
-                
-                if(queryFunction(element)) {
-                    results.push(element);
-                }
-                
-                iterate(element);
+
+        iterator.walkChildren(root, function(element) {
+            if(queryFunction(element)) {
+                results.push(element);
             }
-        };
-        
-        iterate(root);
-            
+
+            if(limit && results.length >= limit) {
+                return false;
+            }
+        });
+
         return results;
     };
-    
+
     var getElement = function(root, query) {
         return getElements(root, query, 1);
     };
-    
+
     var getParents = function(root, query, limit) {
         var queryFunction = getMatchingFunction(query),
-            elements = [root],
             results = [];
-        
-        while(elements = iterator.parents(elements[0])) {
-            if(elements.length < 1) {
-                break;
+
+        iterator.walkParents(root, function(element) {
+            if(queryFunction(element)) {
+                results.push(element);
             }
-            
+
             if(limit && results.length >= limit) {
-                break;
+                return false;
             }
-            
-            if(queryFunction(elements[0])) {
-                results.push(elements[0]);
-            }
-        }
-        
+        });
+
         return results;
     };
-    
+
     var getParent = function(root, query) {
         return getParents(root, query, 1);
     };
-    
+
     var selector = getElements;
     selector.getElements = getElements;
     selector.getElement = getElement;
     selector.getParents = getParents;
     selector.getParent = getParent;
-    
+
     return selector;
 }());
 
