27e522b9ba10f49b448fd820235ee1f3a66dd6e7
kchapelier <kevin.chapelier@gmail.com>
2014-10-14 13:09:35 +0200

    simplify the ~= operator

--- src/lib/operators.js
+++ src/lib/operators.js
@@ -1,11 +1,6 @@
 var operators = (function() {
     "use strict";
 
-    //From http://stackoverflow.com/a/6969486
-    var escapeRegexpString = function(str) {
-        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    };
-
     var list = {
         'match-tag' : function(actual, expected) {
             actual = actual.substr(actual.lastIndexOf('.') + 1);
@@ -16,7 +11,7 @@ var operators = (function() {
             return (actual === expected);
         },
         '~=' : function(actual, expected) {
-            return ((new RegExp('(^| )' + escapeRegexpString(expected) + '($| )')).test(actual));
+            return (' ' + actual +  ' ').indexOf(' ' + expected + ' ') > -1;
         },
         '|=' : function(actual, expected) {
             return (actual === expected) | (actual.indexOf(expected + '-') === 0);
@@ -25,7 +20,6 @@ var operators = (function() {
             return (actual.indexOf(expected) === 0);
         },
         '$=' : function(actual, expected) {
-
             return (actual.lastIndexOf(expected) === actual.length - expected.length);
         },
         '*=' : function(actual, expected) {
